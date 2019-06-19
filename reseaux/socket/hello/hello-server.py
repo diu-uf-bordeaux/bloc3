3727745888d76630575880a2a5cd4899377a3736
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2019-06-19 15:29:06 +0200

    maj

--- reseaux/socket/hello/hello-server.py
+++ reseaux/socket/hello/hello-server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import sys
 import socket
 
 HOST = b''
@@ -7,6 +8,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
 s.listen(0)
+print("Welcome...")
 
 while True:
     sclient, addr = s.accept()
@@ -14,6 +16,8 @@ while True:
     msg = sclient.recv(1024)
     print(msg.decode())
     sclient.sendall(b'goodbye!')
+    # print("Press Enter to close client connection...")
+    # sys.stdin.readline()
     sclient.close()
     print('Disconnected by', addr)
 
