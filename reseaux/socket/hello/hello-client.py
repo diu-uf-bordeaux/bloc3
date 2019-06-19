9dfbbdb62ebf3d3c914daae660495c7eec665dd9
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2019-06-19 12:19:05 +0200

    add hello example

---
+++ reseaux/socket/hello/hello-client.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+import socket
+
+HOST = b'localhost'
+PORT = 7777
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+addr = s.getpeername()
+print('Connected to', addr)
+s.sendall(b'hello world!')
+msg = s.recv(1024)
+print(msg.decode())
+s.close()
+print('Disconnected from', addr)
+
